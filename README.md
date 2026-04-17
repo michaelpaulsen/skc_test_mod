bd6d02b4ca5ea3197efa18b5e022e0f44f1701c3
michaelpaulsen <michael.j.paulsen@gmail.com>
2026-04-17 13:30:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# skc_test_mod
+a simple mod for Minecraft 1.21.1
---
+++ src/main/java/net/mpaulsen/mods/skeletoncraftstestmodClient.java
@@ -0,0 +1,32 @@
+package net.mpaulsen.mods;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = skeletoncraftstestmod.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = skeletoncraftstestmod.MODID, value = Dist.CLIENT)
+public class skeletoncraftstestmodClient {
+    public skeletoncraftstestmodClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        skeletoncraftstestmod.LOGGER.info("=======================");
+        skeletoncraftstestmod.LOGGER.info(" SKC's TEST MOD LOADED ");
+        skeletoncraftstestmod.LOGGER.info("=======================");
+    }
+}
